f0ae21dabc5356615a1b66035bd427e89c54f20b
Pierre Gaimard <pierre@gaimard-family.fr>
2020-12-18 08:28:59 +0100

    Improve app.js code style

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,8 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+// scss
+import "./styles/app.scss";
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+// Stimulus
+import "./bootstrap";
 
-// start the Stimulus application
-import './bootstrap';
-
-import 'bootstrap';
+// Bootstrap
+import "bootstrap";
