ea2897b2e14e6fa7abd8d64a129024bb963c1cb5
Pierre Gaimard <pierre@gaimard-family.fr>
2020-12-22 21:29:58 +0100

    Fix code style in app.js

--- assets/app.js
+++ assets/app.js
@@ -9,10 +9,10 @@ import "@popperjs/core";
 // Bootstrap
 import "bootstrap";
 
-import Toast from 'bootstrap/js/dist/toast';
+import Toast from "bootstrap/js/dist/toast";
 
 // Toasts init
-let toasts = document.getElementsByClassName('toast');
+let toasts = document.getElementsByClassName("toast");
 for (let toastElm of toasts) {
     let toast = new Toast(toastElm);
     toast.show();
