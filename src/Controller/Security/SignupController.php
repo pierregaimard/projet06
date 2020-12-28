7154946bad99489e55d39d77fc52373afa933208
Pierre Gaimard <pierre@gaimard-family.fr>
2020-12-28 17:34:16 +0100

    Add base form logic to Signup Controller

--- src/Controller/Security/SignupController.php
+++ src/Controller/Security/SignupController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Security;
 
+use App\Entity\User;
+use App\Form\SignupType;
+use App\Service\Notification\Notification;
+use App\Service\Notification\NotificationManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +16,35 @@ class SignupController extends AbstractController
     /**
      * @Route("/sign-up", name="sign-up")
      *
+     * @param Request             $request
+     * @param NotificationManager $notification
+     *
      * @return Response
      */
-    public function signUp(): Response
+    public function signUp(Request $request, NotificationManager $notification): Response
     {
-        return $this->render('security/signup/signup.html.twig');
+        $user = new User();
+        $form = $this->createForm(SignupType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
+
+            $notification->add(new Notification(
+                'Your account has been created!. We sent you an email to confirm your subscription.',
+                Notification::TYPE_SUCCESS
+            ));
+            $notification->dispatch();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render(
+            'security/signup/signup.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
     }
 }
