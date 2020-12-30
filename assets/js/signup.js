27b314bde40f0fe82c34ab30ae29d5759d73d3f1
Pierre Gaimard <pierre@gaimard-family.fr>
2020-12-30 12:22:11 +0100

    Fix code style in signup.js

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,17 +1,13 @@
 import "../styles/signup.scss";
 import "../styles/form.scss";
 
-(function () {
-    'use strict'
+let form = document.getElementById("app-login");
 
-    let form = document.getElementById("app-login")
+form.addEventListener("submit", function (event) {
+    if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
 
-    form.addEventListener("submit", function (event) {
-        if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-        }
-
-        form.classList.add("was-validated")
-    }, false)
-})()
+    form.classList.add("was-validated");
+}, false);
