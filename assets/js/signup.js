9bf68c47191ca48674c4ced2c7551f932c6e3dcf
Pierre Gaimard <pierre@gaimard-family.fr>
2020-12-30 12:15:38 +0100

    Add form control to signup.js entry point

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,2 +1,17 @@
 import "../styles/signup.scss";
 import "../styles/form.scss";
+
+(function () {
+    'use strict'
+
+    let form = document.getElementById("app-login")
+
+    form.addEventListener("submit", function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+
+        form.classList.add("was-validated")
+    }, false)
+})()
