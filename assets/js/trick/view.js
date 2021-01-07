0e70d3fbade4b64d8f8d89809ac30f5a370e5903
Pierre Gaimard <pierre@gaimard-family.fr>
2021-01-07 20:57:34 +0100

    Fix code style in trick/view.js

--- assets/js/trick/view.js
+++ assets/js/trick/view.js
@@ -7,13 +7,13 @@ const modalImage = document.getElementById("trick-view-image-modal-image");
 imgButtons.forEach((element) => {
     element.addEventListener("click", () => {
         modalImage.setAttribute("src", element.dataset.image);
-    })
+    });
 });
 
 // Comments manager
 const commentsContainer = document.getElementById("trick-comments-container");
 const loadMore = document.getElementById("load-more-comments");
-const limit = parseInt(loadMore.dataset.limit);
+const limit = parseInt(loadMore.dataset.limit, 10);
 const loadRoute = commentsContainer.getAttribute("data-route-load");
 const deleteRoute = commentsContainer.getAttribute("data-route-delete");
 
