fa0f729e7f905f955b476bb19295d50c36a4a3f8
Pierre Gaimard <pierre@gaimard-family.fr>
2021-01-08 17:58:05 +0100

    Add headingForm to edit route into TrickEditController

--- src/Controller/Trick/TrickEditController.php
+++ src/Controller/Trick/TrickEditController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Trick;
 
 use App\Entity\Trick;
+use App\Form\TrickHeadingImageType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TrickEditController extends AbstractController
@@ -15,17 +17,40 @@ class TrickEditController extends AbstractController
      *
      * @param $slug
      */
-    public function edit($slug)
+    public function edit($slug, Request $request)
     {
         $manager = $this->getDoctrine()->getManager();
         $trick = $manager->getRepository(Trick::class)->findOneBy(['slug' => $slug]);
+
+        # Heading image form
+        $headingForm = $this->createForm(TrickHeadingImageType::class, $trick, [
+            'image_choices' => $trick->getImages()
+        ]);
+        $headingForm->handleRequest($request);
+        if ($headingForm->isSubmitted() && $headingForm->isValid()) {
+            $manager->flush();
+        }
 
         return $this->render(
             'trick/edit.html.twig',
             [
                 'trick' => $trick,
-                'action' => self::ACTION
+                'action' => self::ACTION,
+                'headingForm' => $headingForm->createView()
             ]
         );
+    }
+
+    /**
+     * @Route("/tricks/update/remove-heading-image/{id}", name="trick_remove_heading_image")
+     */
+    public function removeHeadingImage($id)
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $trick   = $manager->getRepository(Trick::class)->find($id);
+        $trick->setHeadingImage(null);
+        $manager->flush();
+
+        return $this->redirectToRoute('trick_edit', ['slug' => $trick->getSlug()]);
     }
 }
