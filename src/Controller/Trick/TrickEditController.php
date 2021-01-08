ce5889da106d9b28fc389d4fe7160ef9eef9c2c5
Pierre Gaimard <pierre@gaimard-family.fr>
2021-01-08 15:52:12 +0100

    Add trick edit route to TrickEditController

--- src/Controller/Trick/TrickEditController.php
+++ src/Controller/Trick/TrickEditController.php
@@ -1,10 +1,31 @@
 <?php
 
-
 namespace App\Controller\Trick;
 
+use App\Entity\Trick;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class TrickEditController
+class TrickEditController extends AbstractController
 {
     public const ACTION = 'edit';
+
+    /**
+     * @Route("/tricks/edit/{slug}", name="trick_edit")
+     *
+     * @param $slug
+     */
+    public function edit($slug)
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $trick = $manager->getRepository(Trick::class)->findOneBy(['slug' => $slug]);
+
+        return $this->render(
+            'trick/edit.html.twig',
+            [
+                'trick' => $trick,
+                'action' => self::ACTION
+            ]
+        );
+    }
 }
