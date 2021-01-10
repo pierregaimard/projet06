6e4c7774b2ae7037a260f53cf08d790d4ebd4973
Pierre Gaimard <pierre@gaimard-family.fr>
2021-01-10 12:06:41 +0100

    Update validation constraints into Trick entity

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -7,9 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=TrickRepository::class)
+ * @UniqueEntity("name", message="This trick name already exists", groups={"update"})
  */
 class Trick
 {
@@ -100,7 +102,7 @@ class Trick
         return $this->name;
     }
 
-    public function setName(string $name): self
+    public function setName(?string $name): self
     {
         $this->name = $name;
 
@@ -136,7 +138,7 @@ class Trick
         return $this->getDescription();
     }
 
-    public function setDescription(string $description): self
+    public function setDescription(?string $description): self
     {
         $this->description = $description;
 
