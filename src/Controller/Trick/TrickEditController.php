ce823927469329e006d1f3acc4f4131adc96a54c
Pierre Gaimard <pierre@gaimard-family.fr>
2021-01-11 08:18:46 +0100

    Add imageForm feature to TrickEditController

--- src/Controller/Trick/TrickEditController.php
+++ src/Controller/Trick/TrickEditController.php
@@ -3,11 +3,14 @@
 namespace App\Controller\Trick;
 
 use App\Entity\Trick;
+use App\Entity\TrickImage;
 use App\Form\TrickEditType;
 use App\Form\TrickHeadingImageType;
+use App\Form\TrickImageType;
 use App\Service\Notification\Notification;
 use App\Service\Notification\NotificationManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -57,6 +60,11 @@ class TrickEditController extends AbstractController
             return $this->redirectToRoute('trick_view', ['slug' => $trick->getSlug()]);
         }
 
+        # ImageForm
+        $image = new TrickImage();
+        $imageForm = $this->createForm(TrickImageType::class, $image);
+        $this->manageImageForm($image, $imageForm, $request, $trick, $notification);
+
         return $this->render(
             'trick/edit.html.twig',
             [
@@ -64,8 +72,49 @@ class TrickEditController extends AbstractController
                 'edit' => true,
                 'headingForm' => $headingFormView,
                 'infoForm' => $infoForm->createView(),
+                'imageForm' => $imageForm->createView(),
             ]
         );
+    }
+
+    /**
+     * @param TrickImage          $image
+     * @param FormInterface       $imageForm
+     * @param Request             $request
+     * @param Trick               $trick
+     * @param NotificationManager $notification
+     */
+    private function manageImageForm(
+        TrickImage $image,
+        FormInterface $imageForm,
+        Request $request,
+        Trick $trick,
+        NotificationManager $notification
+    ) {
+        $manager = $this->getDoctrine()->getManager();
+
+        $imageForm->handleRequest($request);
+        if ($imageForm->isSubmitted() && $imageForm->isValid()) {
+            $idImage = $request->request->get('idImage');
+            $message = 'The image has been added successfully';
+
+            # If changes an existing image
+            if ($idImage !== "") {
+                $file = $image->getUploadedFile();
+                $image = $manager->getRepository(TrickImage::class)->find($idImage);
+                $image->setUploadedFile($file);
+                $message = 'The image has been updated successfully';
+            }
+
+            $image->setTrick($trick);
+            $manager->persist($image);
+            $manager->flush();
+
+            $trick->addImage($image);
+
+            $notification->add(new Notification($message, Notification::TYPE_SUCCESS));
+            $notification->dispatch();
+        }
     }
 
     /**
@@ -90,6 +139,7 @@ class TrickEditController extends AbstractController
      *
      * @param $type
      * @param $id
+     * @param NotificationManager $notification
      *
      * @return Response
      *
