ad87a02368b1678a7aebb00665cafb368d727ce0
Pierre Gaimard <pierre@gaimard-family.fr>
2021-01-12 08:00:22 +0100

    Rename TrickController to TrickAddController

--- src/Controller/Trick/TrickAddController.php
+++ src/Controller/Trick/TrickAddController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-class TrickController extends AbstractController
+class TrickAddController extends AbstractController
 {
     /**
      * @Route("/tricks/add", name="trick_add")
