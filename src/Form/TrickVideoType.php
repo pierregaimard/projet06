323c16da1214ac688e6756a7d427b8985b48f348
Pierre Gaimard <pierre@gaimard-family.fr>
2021-01-12 08:01:53 +0100

    Add attributes to tag into TrickVideoType FormType.

--- src/Form/TrickVideoType.php
+++ src/Form/TrickVideoType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\TrickVideo;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,10 @@ class TrickVideoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $options = null;
-        $builder->add('tag');
+        $builder->add('tag', TextType::class, [
+            'label' => 'Tag',
+            'label_attr' => ['class' => 'text-dark'],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
