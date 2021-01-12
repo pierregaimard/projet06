550de327319d49e18490a8cf8922ea28fc787b82
Pierre Gaimard <pierre@gaimard-family.fr>
2021-01-12 21:29:29 +0100

    Add postRemove listener to UserListener

--- src/EventListener/UserListener.php
+++ src/EventListener/UserListener.php
@@ -3,6 +3,7 @@
 namespace App\EventListener;
 
 use App\Entity\User;
+use App\Service\Account\AccountImageManager;
 use App\Service\Security\User\UserManager;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 
@@ -14,13 +15,20 @@ class UserListener
     private UserManager $userManager;
 
     /**
+     * @var AccountImageManager
+     */
+    private AccountImageManager $imageManager;
+
+    /**
      * UserListener constructor.
      *
-     * @param UserManager $userManager
+     * @param UserManager         $userManager
+     * @param AccountImageManager $imageManager
      */
-    public function __construct(UserManager $userManager)
+    public function __construct(UserManager $userManager, AccountImageManager $imageManager)
     {
         $this->userManager = $userManager;
+        $this->imageManager = $imageManager;
     }
 
     /**
@@ -31,5 +39,15 @@ class UserListener
     {
         unset($event);
         $this->userManager->initialize($user);
+    }
+
+    /**
+     * @param User               $user
+     * @param LifecycleEventArgs $event
+     */
+    public function postRemove(User $user, LifecycleEventArgs $event)
+    {
+        unset($event);
+        $this->imageManager->remove($user);
     }
 }
